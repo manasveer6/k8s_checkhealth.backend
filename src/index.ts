8077ea43e864355806db79696b225e70db59f262
manasveer6 <manasveer24@gmail.com>
2024-08-19 15:02:54 +0530

    cors fix

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,46 @@
 import express, { Request, Response } from "express";
 import { config } from "dotenv";
 import { initCronJobs } from "./cron/cronJobs";
-// import { listDeployments } from "./lib";
+import { listDeploymentsStatus } from "./lib/kubernetes";
+import cors from "cors";
 
 // Load environment variables from .env file
 config();
 
 const app = express();
 const port = process.env.PORT || 9087;
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
 
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      console.log(origin);
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  }),
+);
 // Initialize cron jobs
 initCronJobs();
 
 // Define API routes
-// app.get("/api/pods-health", async (req: Request, res: Response) => {
-//   try {
-//     const deployments = await listDeployments();
-//     res.json(deployments);
-//   } catch (error) {
-//     console.error("Error fetching pod health:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
+app.get("/api/pods-health", async (req: Request, res: Response) => {
+  console.log("Fetching pod health...");
+  try {
+    const deployments = await listDeploymentsStatus();
+    res.json(deployments);
+  } catch (error) {
+    console.error("Error fetching pod health:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.get("/health", (req: Request, res: Response) => {
   res.send("OK");
